1aa9511d34ab8839df04e6e6527e6044e0341943
mahendra2448 <mahendrasyathi@gmail.com>
2021-07-13 00:33:19 +0700

    ARTI-008: Add FontAwesome and change metas

--- resources/views/frontend/includes/arti-metastyles.blade.php
+++ resources/views/frontend/includes/arti-metastyles.blade.php
@@ -1,7 +1,15 @@
 <meta charset="UTF-8">
+<meta name="theme-color" content="#1D4B6D"/>
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="YAYASAN ARTI adalah sebuah organisasi non-profit yang berdiri sejak tahun 2001. ARTI lahir dari kepedulian para akademis, pendidik, pegiat hak anak, dan aktivis sosial untuk menciptakan kepedulian masyarakat Indonesia terhadap hak-hak dan kesejahteraan anak, serta penghormatan terhadap martabat manusia.">
-<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="keywords" content="yayasan, foundation, yayasan arti, action, research, training, institute, capacity, building, child, childrens, mental health, development, trainers, yatim piatu, yatim, tanggerang, banten">
+<meta name="author" content="YAYASAN ARTI" />
+<meta property="og:type" content="article">
+<meta property="og:title" content="YAYASAN ARTI">
+<meta property="og:site_name" content="YAYASAN ARTI">
+<meta property="og:url" content="https://yayasanarti.com/">
+<meta property="og:description" content="YAYASAN ARTI adalah sebuah organisasi non-profit yang berdiri sejak tahun 2001. ARTI lahir dari kepedulian para akademis, pendidik, pegiat hak anak, dan aktivis sosial untuk menciptakan kepedulian masyarakat Indonesia terhadap hak-hak dan kesejahteraan anak, serta penghormatan terhadap martabat manusia.">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 <title>@yield('title')</title>
@@ -13,6 +21,7 @@
 
 <link rel="canonical" href="{{route('frontend.index')}}">>
 <link rel="stylesheet" href="{{url('/libs/bootstrap-4.6.0/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 <link rel="stylesheet" href="{{url('css/fonts/fonts.css') }}">
 <link rel="stylesheet" href="{{url('css/style.css?v=13246525412') }}">
 <link rel="stylesheet" href="{{url('/libs/slick-1.8.1/slick/slick.css') }}">
